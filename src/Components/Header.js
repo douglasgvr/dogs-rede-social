b7079dca963eb55bf64e8a258015df4287cb09c3
douglasgvr <douglasfarmaangra@gmail.com>
2023-04-26 17:18:43 -0300

    estilos do header finalizado

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,16 @@
 import React from "react";
-import headerStyle from "./Header.module.css";
+import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
-import LogoDogs from "../Assets/dogs.svg";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 
 function Header() {
   return (
-    <header className={headerStyle.header}>
-      <nav className={`${headerStyle.nav} container`}>
-        <Link className={headerStyle.logo} to="/">
-          <img src={LogoDogs} alt="Logo do Site" />
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/">
+          <Dogs />
         </Link>
-        <Link className={headerStyle.login} to="/login">
+        <Link className={styles.login} to="/login">
           Login / Criar
         </Link>
       </nav>
