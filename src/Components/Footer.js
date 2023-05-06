8f9c5a41135317517e9cb0417c44a4b7ab68425d
douglasgvr <douglasfarmaangra@gmail.com>
2023-05-06 16:25:17 -0300

    criando a pagina feed e os estilos

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 function Footer() {
-  return <footer className={styles.rodape}>Footer</footer>;
+  return <footer className={styles.rodape}></footer>;
 }
 
 export default Footer;
